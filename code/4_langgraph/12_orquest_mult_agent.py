06cdb808ca9b1c2515638082fbfbe52d9cc12e9d
Daiha-Helder <daihahelder@gmail.com>
2026-09-11 06:36:19 -0300

    orquestrate_multi_agent

--- code/4_langgraph/12_orquest_mult_agent.py
+++ code/4_langgraph/12_orquest_mult_agent.py
@@ -8,6 +8,8 @@ from langgraph.checkpoint.sqlite import SqliteSaver
 from langchain_core.messages import SystemMessage, HumanMessage
 from pydantic import BaseModel
 from tavily import TavilyClient
+from rich.markdown import Markdown
+
 
 
 import os
@@ -94,6 +96,113 @@ def research_plan_node(state: AgentState):
         response = tavily.search(q, max_results=2)
         for r in response['results']:
             content.append(r['content'])
+    return {"content": content}
+
+
+def generation_node(state: AgentState):
+    content = "\n\n".join(state['content'] or [])
+
+    user_message = HumanMessage(
+        content=f"{state['task']}\n\n\Here is my plan:\n\n{state['plan']}"
+    )
+
+    system_message = SystemMessage(
+        content=WRITER_PROMPT.format(content=content)
+    )
+
+    messages = [system_message, user_message]
+    response = model.invoke(messages)
+
+    return {
+        "draft": response.content,
+        "revision_number": state.get("revision_number", 1) + 1
+    }
+
+def reflection_node(state: AgentState):
+    message = [
+        SystemMessage(content=REFLECTION_PROMPT),
+        HumanMessage(content=state['draft'])
+    ]
+    response = model.invoke(message)
+    return {"critique": response.content}
+
+def research_critique_node(state: AgentState):
+
+    queries = model.with_structured_output(Queries).invoke([
+        SystemMessage(content=RESEARCH_CRITIQUE_PROMPT),
+        HumanMessage(content=state['critique'])
+    ])
+
+    content = state['content'] or []
+
+    for q in queries.queries:
+        response = tavily.search(
+            query=q, 
+            max_results=2
+        )
+        for r in response['results']:
+            content.append(r['content'])
 
     return {"content": content}
 
+def should_continue(state):
+    if state['revision_number'] > state['max_revisions']:
+        return END
+    return "reflect"
+
+#%% Construindo o grafo
+
+builder = StateGraph(AgentState)
+
+# Declarando os nós
+builder.add_node("planner", plan_node)
+builder.add_node("generate", generation_node)
+builder.add_node("reflect", reflection_node)
+builder.add_node("research_plan", research_plan_node)
+builder.add_node("research_critique", research_critique_node)
+
+# Montando a estrutura do grafo
+builder.set_entry_point("planner")
+
+builder.add_edge("planner", "research_plan")
+builder.add_edge("research_plan", "generate")
+
+builder.add_conditional_edges(
+    "generate", 
+    should_continue, 
+    {END: END, "reflect": "reflect"}
+)
+
+builder.add_edge("reflect", "research_critique")
+builder.add_edge("research_critique", "generate")
+graph = builder.compile(checkpointer=memory)
+
+# Salvar como PNG
+png = graph.get_graph().draw_mermaid_png()
+
+with open("./data/graph.png", "wb") as f:
+    f.write(png)
+
+thread = {"configurable": {"thread_id":"1"}}
+for s in graph.stream({
+             'task':'Qual é a diferença entre o langchain e langsmith',
+             'max_revisions': 2,
+             'revision_number': 1,
+             'content': []
+             }, thread):
+  
+    print(s)
+
+# with open("./data/output.md", "w", encoding="utf-8") as f:
+
+#     for s in graph.stream({
+#         "task": "Qual é a diferença entre o langchain e langsmith",
+#         "max_revisions": 2,
+#         "revision_number": 1,
+#         "content": []
+#     }, thread):
+
+#         f.write(f"## Node\n\n")
+#         f.write("```python\n")
+#         f.write(f"{s}\n")
+#         f.write("```\n\n")
